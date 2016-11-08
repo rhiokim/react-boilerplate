7281a78ea599fc1e1d71b77f572ae11a536d60bf
rhiokim <rhio.kim@gmail.com>
2016-11-08 16:07:45 +0900

    feat(pkg): add react-toolbox! but it doesn't work to build for production

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Button} from 'react-toolbox';
 
 export default class Home extends React.Component {
   render() {
     return (
       <div>
         <h1>Home</h1>
+        <Button icon='bookmark' label='Bookmark' accent />
+        <Button icon='bookmark' label='Bookmark' raised primary />
       </div>
     );
   }
