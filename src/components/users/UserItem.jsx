bcf3867ed1fd285b50cbabb5d96d523e1f31c293
rhiokim <rhio.kim@gmail.com>
2017-03-27 18:36:49 +0900

    feat(component): improve flowtype of component code

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,6 +1,19 @@
+/* @flow */
 import React from 'react';
 
 export default class UserItem extends React.Component {
+  props: {
+    id: number,
+    name: string,
+    username: string,
+    email: string,
+    address: Address,
+    phone: string,
+    website: string,
+    company: Company,
+    onClick?: (event: Event) => void
+  };
+
   render() {
     const {name, email} = this.props;
 
